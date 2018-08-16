e6423a2a2aaf526bf7712f0f6828ac7bbe5ee31b
Daniele Atzeni <atzeni.daniele@posteo.eu>
2018-08-16 16:40:22 +0200

    Creato thread per la lettura delle news

--- src/commons/Reader.java
+++ src/commons/Reader.java
@@ -4,21 +4,24 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.TimeUnit;
 
 class Reader implements Runnable {
-	   private final ConcurrentLinkedQueue<NewsInterface> queue;
-	   Reader(ConcurrentLinkedQueue<NewsInterface> q) { queue = q; }
-	   public void run() {
-	     try {
-	       while (true) { 
-	    	   TimeUnit.SECONDS.sleep(10);
-	    	   if(!queue.isEmpty())
-	    		   while(!queue.isEmpty()) {
-	    	    	   NewsInterface n= queue.poll(); 
-	    	    	   System.out.println("Topic: "+n.GetTopic()+ "\\n Testo: "+n.GetText());
-	    		   }
+	private final ConcurrentLinkedQueue<NewsInterface> queue;
 
-	    	   }
-	     } catch (InterruptedException ex) {
-	    	 ex.printStackTrace();
-	     }
-	   }
-	 }
+	Reader(ConcurrentLinkedQueue<NewsInterface> q) {
+		queue = q;
+	}
+
+	public void run() {
+		try {
+			while (true) {
+				TimeUnit.SECONDS.sleep(10);
+				if (!queue.isEmpty())
+					while (!queue.isEmpty()) {
+						NewsInterface n = queue.poll();
+						System.out.println("Topic: " + n.GetTopic() + "\\n Testo: " + n.GetText());
+					}
+			}
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+	}
+}
